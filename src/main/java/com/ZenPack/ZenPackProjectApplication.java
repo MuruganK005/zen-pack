43ce85231616699a87657b73dbadc5b20953dc61
vtguser <muruganeee005@gmai.com>
2022-10-14 19:33:38 +0530

    Final Commit

--- src/main/java/com/ZenPack/ZenPackProjectApplication.java
+++ src/main/java/com/ZenPack/ZenPackProjectApplication.java
@@ -1,16 +1,26 @@
 package com.ZenPack;
 
+import com.ZenPack.utils.DataSourceConnection;
+import com.ZenPack.utils.ZKModel;
+import com.ZenPack.utils.ZookeeperConnection;
+import org.apache.zookeeper.KeeperException;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.event.EventListener;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import javax.swing.text.Utilities;
+import java.io.IOException;
+
 //
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 @EnableSwagger2
@@ -29,6 +39,16 @@ public class ZenPackProjectApplication{
 	@Bean
 	public ModelMapper modelMapper(){
 		return new ModelMapper();
+	}
+	@EventListener(ApplicationReadyEvent.class)
+	public void doSomethingAfterStartup() throws IOException, KeeperException, InterruptedException
+	{
+		ZookeeperConnection zkConnection = new ZookeeperConnection();
+		ZKModel.zkData = zkConnection.getZKData();
+
+		Utilities utilites = new Utilities();
+		DataSourceConnection dataSource = new DataSourceConnection();
+
 	}
 /*	@Bean
 	public Jackson2ObjectMapperBuilder objectMapperBuilder() {
