87ec2b2b00c6a91a0c7b45e7e8a28d846e1cd5ae
Java Team <javateam@virtualtechgurus.com>
2022-10-18 07:46:57 -0500

    ZenPackProjectApplication.java edited online with Bitbucket

--- src/main/java/com/ZenPack/ZenPackProjectApplication.java
+++ src/main/java/com/ZenPack/ZenPackProjectApplication.java
@@ -19,6 +19,8 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.swing.text.Utilities;
 import java.io.IOException;
