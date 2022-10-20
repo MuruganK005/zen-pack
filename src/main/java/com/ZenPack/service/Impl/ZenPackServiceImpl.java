b51e641a3aa1473827816d8faa40b2bcc74b9351
Java Team <javateam@zenfra.co>
2022-10-20 21:16:23 +0530

    Report header added

--- src/main/java/com/ZenPack/service/Impl/ZenPackServiceImpl.java
+++ src/main/java/com/ZenPack/service/Impl/ZenPackServiceImpl.java
@@ -20,7 +20,9 @@ import org.springframework.stereotype.Service;
 import com.ZenPack.Dto.ZenPackDto;
 import com.ZenPack.Specification.SearchRequest;
 import com.ZenPack.Specification.SearchSpecification;
+import com.ZenPack.model.ReportHeader;
 import com.ZenPack.model.ZenPack;
+import com.ZenPack.repository.ReportHeaderRepository;
 import com.ZenPack.repository.ZenPackRepository;
 import com.ZenPack.service.Services.ZenPackService;
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -31,105 +33,137 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class ZenPackServiceImpl implements ZenPackService {
 
+	@Autowired
+	private ZenPackRepository repository;
 
-    @Autowired
-    private ZenPackRepository repository;
+	@Autowired
+	private ReportHeaderRepository reportHeaderRepo;
 
-    @Autowired
-    private EntityManager manager;
+	@Autowired
+	private EntityManager manager;
 
-    private static final Logger logger=LoggerFactory.getLogger(ZenPackServiceImpl.class);
+	private static final Logger logger = LoggerFactory.getLogger(ZenPackServiceImpl.class);
 
-    @Override
-    public ResponseEntity<ZenPack> saveZenPack(ZenPack zenPack) {
-        repository.save(zenPack);
-        logger.info("Zen-Pack Saved Successfully");
-        return new ResponseEntity<>(zenPack, HttpStatus.CREATED);
-    }
+	@Override
+	public ResponseEntity<ZenPack> saveZenPack(ZenPack zenPack) {
+		repository.save(zenPack);
+		logger.info("Zen-Pack Saved Successfully");
+		return new ResponseEntity<>(zenPack, HttpStatus.CREATED);
+	}
 
-    @Override
-    public ResponseEntity<ZenPackDto> createZenPack(ZenPackDto zenPackDto)  {
-        ModelMapper mapper=new ModelMapper();
-        mapper.getConfiguration().setAmbiguityIgnored(true);
-        ZenPack zenPack=mapper.map(zenPackDto,ZenPack.class);
-        zenPack.setCreatedDate(new Date());
-        zenPack.setUpdatedTime(new Date());
-        repository.save(zenPack);
-        zenPackDto.setZenPackId(zenPack.getZenPackId());
-        /* zenPackDto.setName(zenPack.getName());*/
-        zenPackDto.setCreatedDate(zenPack.getCreatedDate());
-        zenPackDto.setUpdatedTime(zenPack.getUpdatedTime());
-        return new ResponseEntity<>(zenPackDto,HttpStatus.OK);
-    }
+	@Override
+	public ResponseEntity<ZenPackDto> createZenPack(ZenPackDto zenPackDto) {
+		ModelMapper mapper = new ModelMapper();
+		mapper.getConfiguration().setAmbiguityIgnored(true);
+		ZenPack zenPack = mapper.map(zenPackDto, ZenPack.class);
+		zenPack.setCreatedDate(new Date());
+		zenPack.setUpdatedTime(new Date());
+		repository.save(zenPack);
+		zenPackDto.setZenPackId(zenPack.getZenPackId());
+		/* zenPackDto.setName(zenPack.getName()); */
+		zenPackDto.setCreatedDate(zenPack.getCreatedDate());
+		zenPackDto.setUpdatedTime(zenPack.getUpdatedTime());
+		return new ResponseEntity<>(zenPackDto, HttpStatus.OK);
+	}
 
-
-/*         ZenPack zenPack=new ZenPack();
-    ObjectMapper mapper=new ObjectMapper();
-    String gson= new Gson().toJson(createDto);
-    zenPack.setJsonData(mapper.writeValueAsString(createDto));
-    repository.save(zenPack);*/
-   /* ResponseDto responseDto=new ResponseDto();
-    responseDto.setResponseMessage("ZenPack Created Successfully");
-    responseDto.setResponseCode("Ok");
-    responseDto.setStatusCode(200);
-    responseDto.setResponseDescription("ZenPack Created Successfully");
-    responseDto.setData(null);
-    Gson gson1=new Gson();
-    ZenPackDto zenPackDto=gson1.fromJson(gson,ZenPackDto.class);
-    responseDto.setJdata(zenPackDto);*/
-  /*  return responseDto;
-        return new ResponseEntity<>(zenPackDto1, HttpStatus.ACCEPTED);
-    }*/
-    @Override
-    public List<ZenPackDto> getAllZenPack() throws JsonProcessingException {
-        List<ZenPack> zenPacks = repository.findAll();
-        List<ZenPackDto> zenPackDtos = new ArrayList<>();
-        for (ZenPack zenpack : zenPacks) {
-            ZenPackDto zenPackDto = new ZenPackDto();
-            zenPackDto.setZenPackId(zenpack.getZenPackId());
-            zenPackDto.setName(zenpack.getName());
-            zenPackDto.setZenPackId(zenPackDto.getZenPackId());
-            zenPackDto.setMenus(zenpack.getMenus());
+	/*
+	 * ZenPack zenPack=new ZenPack(); ObjectMapper mapper=new ObjectMapper(); String
+	 * gson= new Gson().toJson(createDto);
+	 * zenPack.setJsonData(mapper.writeValueAsString(createDto));
+	 * repository.save(zenPack);
+	 */
+	/*
+	 * ResponseDto responseDto=new ResponseDto();
+	 * responseDto.setResponseMessage("ZenPack Created Successfully");
+	 * responseDto.setResponseCode("Ok"); responseDto.setStatusCode(200);
+	 * responseDto.setResponseDescription("ZenPack Created Successfully");
+	 * responseDto.setData(null); Gson gson1=new Gson(); ZenPackDto
+	 * zenPackDto=gson1.fromJson(gson,ZenPackDto.class);
+	 * responseDto.setJdata(zenPackDto);
+	 */
+	/*
+	 * return responseDto; return new ResponseEntity<>(zenPackDto1,
+	 * HttpStatus.ACCEPTED); }
+	 */
+	@Override
+	public List<ZenPackDto> getAllZenPack() throws JsonProcessingException {
+		List<ZenPack> zenPacks = repository.findAll();
+		List<ZenPackDto> zenPackDtos = new ArrayList<>();
+		for (ZenPack zenpack : zenPacks) {
+			ZenPackDto zenPackDto = new ZenPackDto();
+			zenPackDto.setZenPackId(zenpack.getZenPackId());
+			zenPackDto.setName(zenpack.getName());
+			zenPackDto.setZenPackId(zenPackDto.getZenPackId());
+			zenPackDto.setMenus(zenpack.getMenus());
 //            JsonReader reader = new JsonReader(new StringReader(zenpack.getJsonData()));
 //            reader.setLenient(true);
 //            MenuDto[] userinfo1 = gson.fromJson(reader, MenuDto[].class);
 //            ArrayList<MenuDto> list = new ArrayList(Arrays.asList(userinfo1));
 //            zenPackDto.setMenus(list);
-            zenPackDtos.add(zenPackDto);
-        }
-       return zenPackDtos;
-    }
+			zenPackDtos.add(zenPackDto);
+		}
+		return zenPackDtos;
+	}
 
-    @Override
-    public String deleteByzenPackId(Long zenPackId) {
-        repository.deleteByZenPackId(zenPackId);
-        return  " Id "+zenPackId+" Deleted SuccessFully";
-    }
+	@Override
+	public String deleteByzenPackId(Long zenPackId) {
+		repository.deleteByZenPackId(zenPackId);
+		return " Id " + zenPackId + " Deleted SuccessFully";
+	}
 
-    @Override
-    public ZenPackDto getByZenPackId(Long zenPackId) {
-        Optional<ZenPack> zenPack= repository.findByZenPackId(zenPackId);
-        ModelMapper mapper=new ModelMapper();
-        mapper.getConfiguration().setAmbiguityIgnored(true);
-        ZenPackDto zenPackDto=mapper.map(zenPack,ZenPackDto.class);
+	@Override
+	public ZenPackDto getByZenPackId(Long zenPackId) {
+		Optional<ZenPack> zenPack = repository.findByZenPackId(zenPackId);
+		ModelMapper mapper = new ModelMapper();
+		mapper.getConfiguration().setAmbiguityIgnored(true);
+		ZenPackDto zenPackDto = mapper.map(zenPack, ZenPackDto.class);
 //        JsonReader reader = new JsonReader(new StringReader(zenPack.get().getJsonData()));
 //        reader.setLenient(true);
 //        MenuDto[] userinfo1 = gson.fromJson(reader, MenuDto[].class);
 //        ArrayList<MenuDto> list = new ArrayList(Arrays.asList(userinfo1));
 //        zenPackDto.setMenus(list);
-        return zenPackDto;
-    }
-  
-    @Override
+		return zenPackDto;
+	}
+
+	@Override
 	public boolean checkZenPackName(String name) {
-		boolean exists = repository.findByName(name).size() ==0 ? false:true;
+		boolean exists = repository.findByName(name).size() == 0 ? false : true;
 		return exists;
 	}
-    
-    @Override
+
+	@Override
 	public Page<ZenPack> searchZenPack(SearchRequest request) {
 		SearchSpecification<ZenPack> specification = new SearchSpecification<>(request);
-        Pageable pageable = SearchSpecification.getPageable(request.getPage(), request.getSize());
-        return repository.findAll(specification, pageable);
+		Pageable pageable = SearchSpecification.getPageable(request.getPage(), request.getSize());
+		return repository.findAll(specification, pageable);
+	}
+
+	public ReportHeader createReportHeader(final ReportHeader reportHeader) {
+		return this.reportHeaderRepo.save(reportHeader);
+	}
+
+	public List<ReportHeader> getAllReportHeader() {
+		return this.reportHeaderRepo.findAll();
+	}
+
+	public String deleteReportHeaderById(Long reportId) {
+		this.reportHeaderRepo.deleteById(reportId);
+		return "Deleted Successfully";
+	}
+
+	public ReportHeader getReportHeaderById(Long reportId) {
+		Optional<ReportHeader> reportHeader = this.reportHeaderRepo.findById(reportId);
+		if (reportHeader != null && reportHeader.isPresent()) {
+			return reportHeader.get();
+		}
+		return null;
+	}
+
+	public ReportHeader getReportHeaderByName(String reportName) {
+		Optional<ReportHeader> reportHeader = this.reportHeaderRepo.findByReportName(reportName);
+		if (reportHeader != null && reportHeader.isPresent()) {
+			return reportHeader.get();
+		}
+		return null;
 	}
 }
