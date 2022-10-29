2d13ffb904569610c21291f276727301652c537c
Java Team <>
2022-10-29 18:37:58 +0530

    JWT

--- src/main/java/com/ZenPack/CorsConfig.java
+++ src/main/java/com/ZenPack/CorsConfig.java
@@ -1,11 +1,27 @@
 package com.ZenPack;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.ZenPack.interceptor.JWTInterceptor;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
+	
+	@Autowired
+    private JWTInterceptor authorizationInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        InterceptorRegistration ir = registry.addInterceptor(authorizationInterceptor);
+        ir.addPathPatterns("/**");
+        ir.excludePathPatterns("/exclude");
+    }
+    
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
